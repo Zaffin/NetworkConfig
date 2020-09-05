cffebacf996f5b1378fb16c9f47a3db40bc06b22
Zaffin <42412051+Zaffin@users.noreply.github.com>
2020-09-05 19:36:09 -0400

    Added directory validation

---
+++ NetworkConfig/ValidationRules/DirectoryExistsRule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NetworkConfig.ValidationRules
+{
+    public class DirectoryExistsRule : ValidationRule
+    {
+        public int MinimumCharacters { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string path = value as string;
+
+            if (Directory.Exists(path))
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                return new ValidationResult(false, $"The selected directory does not exist");
+            }                        
+        }
+    }
+}
